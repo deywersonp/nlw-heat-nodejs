90a06dd2d6fb0fcfe76afd4c9a73b9b20da1fc9d
Deywerson Pereira <deywerson.pereira@gmail.com>
2021-10-22 22:35:25 -0300

    Finished configuration to catch users info from Github

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -3,17 +3,28 @@ import axios from 'axios';
 /**
  * Receber code(string)
  * Recuperar o access_token no github
+ * Recuperar os dados do usuário no Github
  * Verificar se o usuário existe no DB
  * ---- SIM = Gera um token
  * ---- NÃO = Cria no DB e gera um token
  * Retornar o token com as infos do usuário
  */
 
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
+interface IUserResponse {
+  avatar_url: string,
+  login: string,
+  id: number,
+  name: string
+}
 class AuthenticateUserService {
   async execute(code: string){
     const url = "https://github.com/login/oauth/access_token";
 
-    const response = await axios.post(url, null, { 
+    const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, { 
       params: {
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -23,6 +34,12 @@ class AuthenticateUserService {
         "Accept": "application/json"
       }
     });
+
+    const response = await axios.get<IUserResponse>("https://api.github.com/user", {
+      headers: {
+        authorization: `Bearer ${ accessTokenResponse.access_token }`
+      }
+    })
 
     return response.data;
   }
